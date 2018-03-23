5da54e71df00a10c3becd411d37d543caecac6ee
Zachary Gilkerson <zgilkerson1290@gmail.com>
2018-03-22 22:20:55 -0500

    create initial django project

---
+++ mixeddit/mixeddit/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for mixeddit project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mixeddit.settings")
+
+application = get_wsgi_application()
