5324c0af9bd8a9b8ff9909ec347fe504882cf410
Zachary Gilkerson <zgilkerson1290@gmail.com>
2018-03-25 20:43:06 -0500

    add plmock playlist_replace url

--- mixeddit/spotify/urls.py
+++ mixeddit/spotify/urls.py
@@ -2,6 +2,11 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'spotify'
 urlpatterns = [
+    # ex: /spotify/
     url(r'^$', views.index, name='index'),
+    # ex: /spotify/metal/TopOfShreddit
+    url(r'^(?P<subreddit>[a-zA-Z0-9_]+)/(?P<playlist>[a-zA-Z0-9_]+)/?$',
+        views.playlist_replace, name='playlist_replace'),
 ]
--- mixeddit/spotify/views.py
+++ mixeddit/spotify/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 
 
 def index(request):
-    return HttpResponse("Hello, this is Spotify.index")
+    return HttpResponse('Hello, this is Spotify.index')
+
+
+def playlist_replace(request, subreddit, playlist):
+    # if 'it' in playlist:
+        # raise Http404('You playlist has a bad name')
+    return HttpResponse('Are you sure you want to replace {playlist} '
+                        'with the top of the past week from r/{subreddit}'
+                        .format(playlist=playlist, subreddit=subreddit))
