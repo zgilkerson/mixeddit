9d998f6ab3d9c412550806120a54977d426ca704
Zachary Gilkerson <zgilkerson1290@gmail.com>
2018-04-03 18:01:34 -0500

    [feat-10] implement login and callback logic

--- mixeddit/settings.py
+++ mixeddit/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sslserver',
     'rest_framework',
     'spotify.apps.SpotifyConfig',
 ]
--- spotify/views.py
+++ spotify/views.py
@@ -1,6 +1,8 @@
 import configparser
 from requests_oauthlib import OAuth2Session
 
+from django.http import HttpResponseRedirect
+
 from rest_framework import status, viewsets
 from rest_framework.decorators import detail_route
 from rest_framework.parsers import JSONParser
@@ -15,5 +17,36 @@ class SpotifyViewSet(viewsets.ViewSet):
 
     @detail_route(methods=['get'])
     def login(self, request, pk):
-        scope = 'playlist-read-private, playlist-modify-private, user-read-private'
-        return Response(status=status.HTTP_200_OK)
+        scope = ('playlist-read-private, playlist-modify-public, '
+                 'playlist-modify-private')
+        config = configparser.ConfigParser()
+        config.read('spotify.ini')
+        client_id = config['spotify']['client_id']
+        redirect_uri = 'https://localhost:8000/spotify/callback'
+        oauth = OAuth2Session(client_id=client_id,
+                              redirect_uri=redirect_uri,
+                              scope=scope)
+        auth_url, state = oauth.authorization_url(
+            'https://accounts.spotify.com/authorize')
+        request.session['oauth_state'] = state
+        return HttpResponseRedirect(auth_url)
+
+    @detail_route(methods=['get'])
+    def callback(self, request, pk):
+        config = configparser.ConfigParser()
+        config.read('spotify.ini')
+        client_id = config['spotify']['client_id']
+        client_secret = config['spotify']['client_secret']
+        redirect_uri = 'https://localhost:8000/spotify/callback'
+        oauth = OAuth2Session(client_id=client_id,
+                              redirect_uri=redirect_uri,
+                              state=request.session['oauth_state'])
+        token_uri = 'https://accounts.spotify.com/api/token'
+        token = oauth.fetch_token(
+            token_uri,
+            authorization_response=request.build_absolute_uri(),
+            client_secret=client_secret)
+        request.session['token'] = token
+        BASE_URL = 'https://api.spotify.com/v1/'
+        response = oauth.get(BASE_URL+'me').json()
+        return Response(response, status=status.HTTP_200_OK)
