156896077fd9fa8efdbe8399871402a5d1afc6ce
Zachary Gilkerson <zgilkerson1290@gmail.com>
2018-05-17 20:03:47 -0500

    [feat-18] break faq page

--- frontend/src/app/faq/faq.component.ts
+++ frontend/src/app/faq/faq.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-faq',
   templateUrl: './faq.component.html',
-  styleUrls: ['./faq.component.css']
+  styleUrls: ['./faq.component.scss']
 })
-export class FaqComponent implements OnInit {
+export class FaqComponent implements OnInit, AfterViewInit {
+  private fragment: string;
+  _sub;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this._sub = this.route.fragment.subscribe((hash: string) => {
+      if (hash) {
+        const cmp = document.getElementById(hash);
+        if (cmp) {
+          cmp.scrollIntoView();
+        }
+      } else {
+        window.scrollTo(0, 0);
+      }
+    });
   }
-
+  
+  // ngAfterViewInit() {
+  //   try {
+  //     document.querySelector('#' + this.fragment).scrollIntoView();
+  //   } catch (e) { }
+  // }
 }
